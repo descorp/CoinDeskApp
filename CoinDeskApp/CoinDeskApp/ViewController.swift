00d7df6864614b25b6a7868004c806f6b4aaa99e
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2018-07-22 18:08:42 +0200

    feature: initial commit

---
+++ CoinDeskApp/CoinDeskApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinDeskApp
+//
+//  Created by Vladimir Abramichev on 22/07/2018.
+//  Copyright © 2018 Vladimir Abramichev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoinDeskProvider/CoinDeskProvider/CoinDeskProvider.h
@@ -0,0 +1,19 @@
+//
+//  CoinDeskProvider.h
+//  CoinDeskProvider
+//
+//  Created by Vladimir Abramichev on 22/07/2018.
+//  Copyright © 2018 Vladimir Abramichev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoinDeskProvider.
+FOUNDATION_EXPORT double CoinDeskProviderVersionNumber;
+
+//! Project version string for CoinDeskProvider.
+FOUNDATION_EXPORT const unsigned char CoinDeskProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinDeskProvider/PublicHeader.h>
+
+
